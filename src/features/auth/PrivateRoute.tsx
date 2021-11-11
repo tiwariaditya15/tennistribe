0ff7850e37c912a142d89604668534c9a4c97214
tiwariaditya15 <tiwariaditya15@hotmail.com>
2021-11-11 12:59:08 +0530

    fix: upgrade to react-router v6

--- src/features/auth/PrivateRoute.tsx
+++ src/features/auth/PrivateRoute.tsx
@@ -1,5 +1,11 @@
+import { Navigate, useLocation } from "react-router-dom";
 import { useAppSelector } from "../../app/hooks";
 
-export default function PrivateRoute() {
+export default function PrivateRoute({ children }: { children: JSX.Element }) {
   const logged = useAppSelector((state) => state.auth.logged);
+  const location = useLocation();
+  if (!logged) {
+    <Navigate to="/signin" state={{ from: location }} replace={true} />;
+  }
+  return children;
 }
